7f475237caa02f62c10f8f30767482b10434361a
jeka <ElectroDeggi33759@yandex.ru>
2017-07-03 13:41:25 +0300

    123

--- app/js/common.js
+++ app/js/common.js
@@ -311,3 +311,8 @@ $('.open-popup-link').magnificPopup({
     type:'inline',
     midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
 });
+
+
+    $('.popup-link').magnificPopup({
+        type: 'image'
+    });
